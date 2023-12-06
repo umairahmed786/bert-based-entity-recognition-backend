2e684ddd5d1e31408f65fdecb291a12f2625f728
Umair Ahmed <umairahmedpaki7@gmail.com>
2023-12-06 15:24:34 +0500

    little changes

--- bert_based_entity_recognition_backend/tokenize.py
+++ bert_based_entity_recognition_backend/tokenize.py
@@ -1,26 +1,45 @@
 
-# from django.views.decorators.csrf import csrf_exemp
-# # @api_view(["POST"])
-# @csrf_exempt 
-# def tokenize(request):
-#     print("hello")
-#     if request.method == "POST":
-#         print("reached")
-#         # return reached
-#         # return HttpResponseRedirect(request('world:Profile'))
+# # from django.views.decorators.csrf import csrf_exemp
+# # # @api_view(["POST"])
+# # @csrf_exempt 
+# # def tokenize(request):
+# #     print("hello")
+# #     if request.method == "POST":
+# #         print("reached")
+# #         # return reached
+# #         # return HttpResponseRedirect(request('world:Profile'))
 
-# ----------------------
+# # ----------------------
+
+# from django.views.decorators.csrf import csrf_exempt
+# from django.views import View
+# from django.http import JsonResponse
+# import pickle
+# from transformers import pipeline
+# from transformers import BertTokenizerFast
+# import json
+
+# class UserAccessView(View):
+#     http_method_names = ["post"]  
+
+#     @csrf_exempt
+#     def dispatch(self, request, *args, **kwargs):
+#         return super().dispatch(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         if "tokenize" in request.path:
+#             return self.tokenize(request)
+#         return Response({"message": "Invalid request."})
 
 from django.views.decorators.csrf import csrf_exempt
 from django.views import View
 from django.http import JsonResponse
 import pickle
-from transformers import pipeline
-from transformers import BertTokenizerFast
+from transformers import pipeline, BertTokenizerFast
 import json
 
 class UserAccessView(View):
-    http_method_names = ["post"]  
+    http_method_names = ["post"]
 
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
@@ -29,7 +48,7 @@ class UserAccessView(View):
     def post(self, request, *args, **kwargs):
         if "tokenize" in request.path:
             return self.tokenize(request)
-        return Response({"message": "Invalid request."})
+        return JsonResponse({"message": "Invalid request."})
 
     def tokenize(self, request):
         if request.method == "POST":
@@ -39,6 +58,18 @@ class UserAccessView(View):
             tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
             nlp = pipeline("ner", model=test_pickle, tokenizer=tokenizer)
             ner_results = nlp(text)
-            return JsonResponse({"message": json.dumps(str(ner_results))})
 
+            # Convert float32 to float for JSON serialization
+            formatted_results = []
+            for entity in ner_results:  
+                formatted_entity = {
+                    "entity": entity["entity"],
+                    "score": float(entity["score"]),  # Convert float32 to float
+                    "index": entity["index"],
+                    "word": entity["word"],
+                    "start": entity["start"],
+                    "end": entity["end"]
+                }
+                formatted_results.append(formatted_entity)
 
+            return JsonResponse({"message": formatted_results})
